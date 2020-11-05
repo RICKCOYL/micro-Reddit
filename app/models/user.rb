a5d07f9d25da3026048d858b574eb8de512d897e
RICKCOYL <rickoburu@gmail.com>
2020-11-05 16:28:34 +0300

    Add user validation

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    validates :name,  presence: true, length: { maximum: 50 }
+    validates :name, :uniqueness => true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+end
