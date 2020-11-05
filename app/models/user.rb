78b6440d948ed4442af39d5aa52196270154c7fe
Daniduran-dev <danigduran@gmail.com>
2020-11-05 21:03:13 +0700

    add associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     validates :name,  presence: true, length: { maximum: 50 }
     validates :name, :uniqueness => true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+    has_many :post, dependent: :destroy
 end
